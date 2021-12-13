b03c72345042425f39ddd8dfe22df9cc983d9df7
brtauhid <br.tauhid@gmail.com>
2021-12-13 14:27:07 +0600

    5th commit

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -4,7 +4,21 @@ import "./intro.css"
 const intro = () => {
     return (
         <div className='i'>
-            <div className="i-left">left</div>
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                    <h2 className="i-intro">Hello, My name is</h2>
+                    <h1 className="i-name">BR Tauhid</h1>
+                    <div className="i-title">
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item">Web Developer</div>
+                            <div className="i-title-item">UI/UX Designer</div>
+                            <div className="i-title-item">Photographer</div>
+                            <div className="i-title-item">Writer</div>
+                            <div className="i-title-item">Content Creator</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className="i-right">right</div>
         </div>
     );
