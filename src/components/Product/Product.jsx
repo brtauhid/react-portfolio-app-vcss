3b60ec2b6f77efb728f7ef1ae139392b26521214
brtauhid <br.tauhid@gmail.com>
2021-12-14 18:34:33 +0600

    product gallery area code start

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,13 @@
+import './Product.css';
+
+import React from 'react';
+
+const Product = () => {
+    return (
+        <div>
+            product compo
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,13 @@
+import './Product.css';
+
+import React from 'react';
+
+const ProductList = () => {
+    return (
+        <div>
+            product list compo
+        </div>
+    );
+};
+
+export default ProductList;
