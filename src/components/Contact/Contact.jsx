f6ccc081087fd6904f42f407375c71ced9ec60f3
brtauhid <br.tauhid@gmail.com>
2021-12-14 22:21:16 +0600

    coding  contact component structure

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,14 @@
+import './Contact.css';
+
+
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className="c">
+            contact
+        </div>
+    );
+};
+
+export default Contact;
