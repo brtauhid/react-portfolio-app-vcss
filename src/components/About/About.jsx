a2bd71fa6e0bed11c29d5a243087a2af215b6d91
brtauhid <br.tauhid@gmail.com>
2021-12-14 17:59:57 +0600

    about section done

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import aboutImg from '../../img/about-img.jpg'
+import aboutImg from '../../img/about-img.jpg';
+import Award from '../../img/award.png';
 import './About.css';
 
 const About = () => {
@@ -16,7 +17,19 @@ const About = () => {
                 <p className="a-sub">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, ullam cumque nemo repudiandae mollitia totam?</p>
 
                 <p className="a-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe quis enim consequatur dolore odio commodi fugit dolores, accusantium laudantium expedita modi minima laboriosam, animi nostrum ipsa earum deserunt porro repudiandae nobis, nisi ducimus eligendi distinctio atque. Earum quae blanditiis placeat!</p>
+                
+                <div className="a-award">
+                    <img src={Award} alt="" className="a-award-img" />
+                    <div className="a-award-texts">
+                        <h4 className="a-award-title">International Developer Award 2020</h4>
+                        <p className="a-award-desc">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam, fugiat.
+                        </p>
+                    </div>
+                </div>
             </div>
+
+            
         </div>
     );
 };
